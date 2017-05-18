eb6a4b9560369f2512f675119b0ee9228fb0c514
intecomd <zhxm68@gmail.com>
2017-05-18 13:08:37 -0400

    some form changes

--- web_server/client/src/Payments/Payments1.js
+++ web_server/client/src/Payments/Payments1.js
@@ -4,33 +4,33 @@ import logo from '../../public/BlockSchoolLogo.png';
 
 const Payments1 = ({ onSubmit }) => (
   <div>
-  <div id="colorstrip">
-   <p id="position">Play <span id="fun">fun</span> with <span id="code">code</span> in Block School</p>
-  </div>
+    <div id="colorstrip">
+     <p id="position">Play <span id="fun">fun</span> with <span id="code">code</span> in Block School</p>
+    </div>
 
-  <div className="box1">
-   <h3 className="l1">Shopping Cart</h3>
-   <h3 className="l2"><span id="pn">60 Classes Package</span><span id="pri">1920</span></h3>
-   <hr id="hr" color="#00C2B1" />
-   <h3 className="l2"><span id="total">Total</span><span id="totalpri">1920</span></h3>
-   <form >
-     Promotion: <input type="text" name="fname" placeholder="Promotion code" />
-   </form>
+    <div className="box1">
+       <h3 className="l1">Shopping Cart</h3>
+       <h3 className="l2"><span id="pn">60 Classes Package</span><span id="pri">1920</span></h3>
+       <hr id="hr" color="#00C2B1" />
+       <h3 className="l2"><span id="total">Total</span><span id="totalpri">1920</span></h3>
+       <form >
+         Promotion: <input type="text" name="fname" placeholder="Promotion code" />
+       </form>
 
-   <form action="/charge" method="post" >
-     <script
-     src="https://checkout.stripe.com/checkout.js"
-     className="stripe-button"
-     data-key="pk_test_QZQj8bP7Zrhw1KNnFq09qZ1e"
-     data-amount="192000"
-     data-name="Block School"
-     data-image={logo}
-     data-description="The best online coding school!"
-     data-locale="auto"
-     data-currency="usd">
-     </script>
-   </form>
-  </div>
+       <form action="/charge" method="post" >
+         <script
+         src="https://checkout.stripe.com/checkout.js"
+         className="stripe-button"
+         data-key="pk_test_QZQj8bP7Zrhw1KNnFq09qZ1e"
+         data-amount="192000"
+         data-name="Block School"
+         data-image={logo}
+         data-description="The best online coding school!"
+         data-locale="auto"
+         data-currency="usd">
+         </script>
+       </form>
+    </div>
   </div>
 
 );
--- web_server/client/src/Price/Price.js
+++ web_server/client/src/Price/Price.js
@@ -16,58 +16,58 @@ const Price = ({ onSubmit }) => (
 
 
    <div>
-    <p id="pricelineNew1">We have the most competitive prices in the industry.</p>
-    <p id="pricelineNew2">Cancel any class within 24 hours to receive full credit.</p>
+      <p id="pricelineNew1">We have the most competitive prices in the industry.</p>
+      <p id="pricelineNew2">Cancel any class within 24 hours to receive full credit.</p>
    </div>
 
     <div>
-    <ul>
-      <div className="priceNew">
-       <li><img id="priceNew1" src={price1} /></li>
-       <li><img id="priceNew2" src={price2} /></li>
-       <li><img id="priceNew3" src={price3} /></li>
-      </div>
-    </ul>
+      <ul>
+        <div className="priceNew">
+         <li><img id="priceNew1" src={price1} /></li>
+         <li><img id="priceNew2" src={price2} /></li>
+         <li><img id="priceNew3" src={price3} /></li>
+        </div>
+      </ul>
     </div>
 
     <div className="priceButtonNew">
-    <div>
-    <form action="/checkout" onSubmit={onSubmit}>
-      <button type="submit" id="priceButtonNew1">Buy</button>
-    </form>
-    </div>
+      <div>
+        <form action="/checkout" onSubmit={onSubmit}>
+          <button type="submit" id="priceButtonNew1">Buy</button>
+        </form>
+      </div>
+
+      <div>
+        <form action="/checkout1" onSubmit={onSubmit}>
+          <button type="submit" id="priceButtonNew2">Buy</button>
+        </form>
+      </div>
 
     <div>
-    <form action="/checkout1" onSubmit={onSubmit}>
-      <button type="submit" id="priceButtonNew2">Buy</button>
-    </form>
-    </div>
-
-    <div>
-    <form action="/checkout2" onSubmit={onSubmit}>
-      <button type="submit" id="priceButtonNew3">Buy</button>
-    </form>
+      <form action="/checkout2" onSubmit={onSubmit}>
+        <button type="submit" id="priceButtonNew3">Buy</button>
+      </form>
     </div>
     </div>
 
 
 
     <div>
-     <p id="pricelineNew3">100% refund policy</p>
-     <p id="pricelineNew4">Bring the second student to split tuition fee</p>
-     <p id="pricelineNew5">Invite more parents to earn credits</p>
-     <p id="pricelineNew6">Purchase $5,900+ to receive shares of the company</p>
+       <p id="pricelineNew3">100% refund policy</p>
+       <p id="pricelineNew4">Bring the second student to split tuition fee</p>
+       <p id="pricelineNew5">Invite more parents to earn credits</p>
+       <p id="pricelineNew6">Purchase $5,900+ to receive shares of the company</p>
      </div>
 
    <div id="colorstripNew1">
-   <ul className="footerNew">
-     <div className="container-fluid">
-      <li><a href="/login">Log in</a></li>
-      <li><a href="#">FAQ</a></li>
-      <li><a href="#">About us</a></li>
-      <li><a href="#">Contact</a></li>
-    </div>
-   </ul>
+     <ul className="footerNew">
+       <div className="container-fluid">
+        <li><a href="/login">Log in</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+      </div>
+     </ul>
    </div>
 
    <div className="copyrightNew">
